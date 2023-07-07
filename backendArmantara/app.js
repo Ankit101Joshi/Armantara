f5924e5cf51b234374d8e40592f01816cb9069b5
Ankit Joshi <ankit101joshi@gmail.com>
2023-07-07 13:02:34 -0400

    Minor update

--- backendArmantara/app.js
+++ backendArmantara/app.js
@@ -29,7 +29,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("DB CONNECTED SUCCESSFULLY!");
+    console.log("DB CONNECTED SUCCESSFULLY!"+process.env.DATABASE);
   });
 
 //My Routes
